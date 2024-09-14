654a44c1de021e5a0e266f06f6ca505c415198ca
mavericb <eric.boniardi@mail.polimi.it>
2024-09-14 14:45:38 -0600

    .

--- docker/start.sh
+++ docker/start.sh
@@ -36,7 +36,7 @@ if [ ! -d "/app/venv/lib/python3.10/site-packages/models" ]; then
 fi
 
 if [ ! -d "/app/venv/lib/python3.10/site-packages/examples" ]; then
-    echo "Error: inputs directory not found!"
+    echo "Error: examples directory not found!"
     exit 1
 fi
 
@@ -45,15 +45,30 @@ if [ ! -d "/app/outputs" ]; then
     mkdir -p /app/outputs
 fi
 
+# Set default values for environment variables if they are not set
+: ${OUTPUT_PREFIX:="/app/outputs/default_output"}
+: ${MODEL_DIRECTORY:="/app/venv/lib/python3.10/site-packages/models"}
+: ${INPUT_PDB:="/app/venv/lib/python3.10/site-packages/examples/input_pdbs/1qys.pdb"}
+: ${NUM_DESIGNS:="3"}
+: ${CONTIGS:="A1-10/20-30/40-50"}
+
+# Print the values being used
+echo "Using the following parameters:"
+echo "OUTPUT_PREFIX: $OUTPUT_PREFIX"
+echo "MODEL_DIRECTORY: $MODEL_DIRECTORY"
+echo "INPUT_PDB: $INPUT_PDB"
+echo "NUM_DESIGNS: $NUM_DESIGNS"
+echo "CONTIGS: $CONTIGS"
+
 # Prepare the command
 CMD="python /app/RFdiffusion/scripts/run_inference.py"
 
-# Add environment variables to the command if they are set
-[[ ! -z "$OUTPUT_PREFIX" ]] && CMD="$CMD inference.output_prefix=$OUTPUT_PREFIX"
-[[ ! -z "$MODEL_DIRECTORY" ]] && CMD="$CMD inference.model_directory_path=$MODEL_DIRECTORY"
-[[ ! -z "$INPUT_PDB" ]] && CMD="$CMD inference.input_pdb=$INPUT_PDB"
-[[ ! -z "$NUM_DESIGNS" ]] && CMD="$CMD inference.num_designs=$NUM_DESIGNS"
-[[ ! -z "$CONTIGS" ]] && CMD="$CMD contigmap.contigs=[$CONTIGS]"
+# Add environment variables to the command
+CMD="$CMD inference.output_prefix=$OUTPUT_PREFIX"
+CMD="$CMD inference.model_directory_path=$MODEL_DIRECTORY"
+CMD="$CMD inference.input_pdb=$INPUT_PDB"
+CMD="$CMD inference.num_designs=$NUM_DESIGNS"
+CMD="$CMD contigmap.contigs=[$CONTIGS]"
 
 # Check if we have command line arguments
 if [ $# -eq 0 ]; then
